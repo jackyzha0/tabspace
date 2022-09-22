4b48cc40362244c8719d8f6931bb1ddcf6b3eb91
Jacky Zhao <j.zhao2k19@gmail.com>
2022-09-22 08:29:02 -0700

    add extensions

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -2,15 +2,49 @@ import React, { useEffect } from 'react';
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { load, save } from './storage';
+import './Editor.css';
+import Focus from '@tiptap/extension-focus';
+import Link from '@tiptap/extension-link';
+import Placeholder from '@tiptap/extension-placeholder';
+import Typography from '@tiptap/extension-typography';
+import Image from '@tiptap/extension-image';
+
+const PLACEHOLDER_TEXT = `Welcome to your TabSpace!
+Treat this as your own little scratchspace in the comfort of your new tab page.
+Markdown-like syntax is supported and notes are saved in real-time.
+
+This is your new digital home, set it up however you'd like!
+`
 
 const Editor = () => {
   const editor = useEditor({
     extensions: [
-      StarterKit,
+      StarterKit.configure({
+        heading: {
+          levels: [1, 2, 3],
+        },
+        codeBlock: {
+          HTMLAttributes: {
+            class: 'highlight'
+          }
+        }
+      }),
+      Focus.configure({
+        mode: 'deepest',
+      }),
+      Link.configure({
+        protocols: ['mailto'],
+      }),
+      Placeholder.configure({
+        placeholder: PLACEHOLDER_TEXT,
+      }),
+      Typography,
+      Image,
     ],
     content: load(),
     onUpdate: ({ editor }) => save(editor.getJSON())
   });
+
 
   useEffect(() => {
     const updateClosure = (event: StorageEvent) => {
@@ -24,6 +58,10 @@ const Editor = () => {
     window.addEventListener('storage', updateClosure);
     return () => window.removeEventListener('storage', updateClosure);
   }, [editor]);
+
+  if (!editor) {
+    return null
+  }
 
   return (
     <EditorContent editor={editor} />
