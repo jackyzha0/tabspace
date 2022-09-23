ea324e6119e2743355db1b693486eeb1cf71f33c
Jacky Zhao <j.zhao2k19@gmail.com>
2022-09-23 08:58:23 -0700

    darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Darkmode from './Darkmode';
 import Editor from './Editor';
 
 function App() {
   return (
     <div className="App">
+      <Darkmode />
       <Editor />
     </div>
   );
