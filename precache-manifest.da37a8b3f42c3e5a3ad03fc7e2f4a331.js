af1c4d48eb473758916968d9704a855f7ddee7b0
jackyzha0 <jackyzha0@users.noreply.github.com>
2022-09-26 05:30:30 +0000

    deploy: 9b1f2d26240d06b312990afb529795e336a2f31f

---
+++ precache-manifest.da37a8b3f42c3e5a3ad03fc7e2f4a331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1d69d2753c5d0175d38",
+    "url": "./static/css/main.76ab5b64.chunk.css"
+  },
+  {
+    "revision": "b1d69d2753c5d0175d38",
+    "url": "./static/js/main.b1d69d27.chunk.js"
+  },
+  {
+    "revision": "f2382c1f73091bd6625b",
+    "url": "./static/js/1.f2382c1f.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "e7edf56496672af79a64fb096c918e3a",
+    "url": "./index.html"
+  }
+];
