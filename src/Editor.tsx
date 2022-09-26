a2cbcf6070d761bdbd495b14621bf2c26c5e619a
Jacky Zhao <j.zhao2k19@gmail.com>
2022-09-25 23:01:04 -0700

    support pasting images

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { DragEvent, useEffect } from 'react';
+import React, { ClipboardEventHandler, DragEvent, useEffect } from 'react';
 import { useEditor, EditorContent } from '@tiptap/react'
 import { Editor as TiptapEditor } from '@tiptap/core'
 import StarterKit from '@tiptap/starter-kit'
@@ -6,20 +6,9 @@ import { load, save } from './storage';
 import './Editor.css';
 import Focus from '@tiptap/extension-focus';
 import Link from '@tiptap/extension-link';
-import Placeholder from '@tiptap/extension-placeholder';
 import Typography from '@tiptap/extension-typography';
 import Image from '@tiptap/extension-image';
 import { TimedTask } from './tasks/TimedTask';
-
-const PLACEHOLDER_TEXT = `Welcome to your TabSpace!
-
-Treat this as your own little scratchspace in the comfort of your new tab page.
-
-- Markdown-like syntax is supported and notes are saved in real-time.
-- To create a new to-do task, write (due <deadline>) in a block.
-
-This is your new digital home, set it up however you'd like!
-`
 
 export interface Tasks {
   due: number, // JS date in milliseconds past epoch
@@ -69,9 +58,6 @@ const Editor = ({ setTasks }: IEditor) => {
       Link.configure({
         protocols: ['mailto'],
       }),
-      Placeholder.configure({
-        placeholder: PLACEHOLDER_TEXT,
-      }),
       Typography,
       Image,
       TimedTask,
@@ -103,20 +89,31 @@ const Editor = ({ setTasks }: IEditor) => {
     return () => window.removeEventListener('storage', updateClosure);
   }, [editor]);
 
+  const insertImage = (img: File) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', (evt) => {
+      if (evt.target && editor) {
+        const uploaded_image = "" + evt.target.result;
+        editor.chain().focus().setImage({ src: uploaded_image }).run()
+      }
+    });
+    reader.readAsDataURL(img);
+  }
+
   const handleDrop = (evt: DragEvent) => {
     evt.preventDefault();
     evt.stopPropagation();
     const fileList = evt.dataTransfer.files;
     if (fileList.length >= 1) {
       const file = fileList[0];
-      const reader = new FileReader();
-      reader.addEventListener('load', (evt) => {
-        if (evt.target && editor) {
-          const uploaded_image = "" + evt.target.result;
-          editor.chain().focus().setImage({ src: uploaded_image }).run()
-        }
-      });
-      reader.readAsDataURL(file);
+      insertImage(file);
+    }
+  }
+
+  const handlePaste: ClipboardEventHandler<HTMLInputElement> = (evt) => {
+    if (evt.clipboardData.files.length > 0) {
+      const file = evt.clipboardData.files[0];
+      insertImage(file);
     }
   }
 
@@ -125,7 +122,7 @@ const Editor = ({ setTasks }: IEditor) => {
   }
 
   return (
-    <EditorContent onDrop={handleDrop} editor={editor} />
+    <EditorContent onDrop={handleDrop} editor={editor} onPaste={handlePaste} />
   )
 }
 
